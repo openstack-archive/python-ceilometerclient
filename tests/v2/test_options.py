72dee0bd52cfc6f39d3e91315e912898be387f5a
Angus Salkeld <asalkeld@redhat.com>
2013-02-26 14:05:23 +1100

    Add resources and meters to the v2 shell
    This also moves the build_url function to a common file.
    
    Change-Id: Ia94f9fa37c83fc756a395a918ad254111951f67b
    Signed-off-by: Angus Salkeld <asalkeld@redhat.com>

---
+++ tests/v2/test_options.py
@@ -0,0 +1,55 @@
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import unittest
+
+from ceilometerclient.v2 import options
+
+
+class BuildUrlTest(unittest.TestCase):
+
+    def test_one(self):
+        url = options.build_url('/', [{'field': 'this',
+                                       'op': 'gt',
+                                       'value': 43}])
+        self.assertEqual(url, '/?q.op=gt&q.value=43&q.field=this')
+
+    def test_two(self):
+        url = options.build_url('/', [{'field': 'this',
+                                       'op': 'gt',
+                                       'value': 43},
+                                      {'field': 'that',
+                                       'op': 'lt',
+                                       'value': 88}])
+        ops = 'q.op=gt&q.op=lt'
+        vals = 'q.value=43&q.value=88'
+        fields = 'q.field=this&q.field=that'
+        self.assertEqual(url, '/?%s&%s&%s' % (ops, vals, fields))
+
+    def test_default_op(self):
+        url = options.build_url('/', [{'field': 'this',
+                                       'value': 43}])
+        self.assertEqual(url, '/?q.op=&q.value=43&q.field=this')
+
+
+class CliTest(unittest.TestCase):
+
+    def test_one(self):
+        ar = options.cli_to_array('this<=34')
+        self.assertEqual(ar, [{'field': 'this','op': 'le','value': '34'}])
+
+    def test_two(self):
+        ar = options.cli_to_array('this<=34;that!=foo')
+        self.assertEqual(ar, [{'field': 'this','op': 'le','value': '34'},
+                              {'field': 'that','op': 'ne','value': 'foo'}])
+
