0a076826c0288e3d0dec1177eff12164e251a201
Dirk Mueller <dirk@dmllr.de>
2013-04-25 17:47:00 +0200

    Sync requirements with openstack-common/requirements
    Allow prettytable 0.7.x specifically. Add testcase.
    Remove setuptools_git from test-requires, already part
    of setup_require
    
    Change-Id: If0d114cc6ec0f84ea75d798adb5ebc424605b22e

---
+++ tests/test_utils.py
@@ -0,0 +1,52 @@
+# Copyright 2013 OpenStack LLC.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
+import cStringIO
+import sys
+import unittest2
+
+from ceilometerclient.common import utils
+
+
+class UtilsTest(unittest2.TestCase):
+
+    def test_prettytable(self):
+        class Struct:
+            def __init__(self, **entries):
+                self.__dict__.update(entries)
+
+        # test that the prettytable output is wellformatted (left-aligned)
+        columns = ['ID', 'Name']
+        val = ['Name1', 'another', 'veeeery long']
+        images = [Struct(**{'id': i ** 16, 'name': val[i]})
+                  for i in range(len(val))]
+
+        saved_stdout = sys.stdout
+        try:
+            sys.stdout = output_dict = cStringIO.StringIO()
+            utils.print_dict({'K': 'k', 'Key': 'Value'})
+
+        finally:
+            sys.stdout = saved_stdout
+
+        self.assertEqual(output_dict.getvalue(), '''\
++----------+-------+
+| Property | Value |
++----------+-------+
+| K        | k     |
+| Key      | Value |
++----------+-------+
+''')
