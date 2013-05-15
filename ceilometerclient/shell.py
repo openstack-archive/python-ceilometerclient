openstack-archive/python-ceilometerclient

commit 453f4d2a4b7c37fee5f3deee16fb0f8938b6f8d1

client does not show version
add support for --version
update openstack.common to bring in latest openstack.common.version

Change-Id: Ic1e66ea1f1aa65039b79902b893d1b2474ecb63e
Fixes:bug1177573

ceilometerclient/shell.py
@@ -21,11 +21,9 @@ import sys
 
 from keystoneclient.v2_0 import client as ksclient
 
+import ceilometerclient
 from ceilometerclient import exc
-from ceilometerclient import client as ceilometerclient
 from ceilometerclient.common import utils
-
-logger = logging.getLogger(__name__)
 
 
 class CeilometerShell(object):
@@ -45,6 +43,10 @@ class CeilometerShell(object):
                             action='store_true',
                             help=argparse.SUPPRESS,
                             )
+
+        parser.add_argument('--version',
+                            action='version',
+                            version=ceilometerclient.__version__)
 
         parser.add_argument('-d', '--debug',
                             default=bool(utils.env('CEILOMETERCLIENT_DEBUG')),

ceilometerclient/__init__.py
@@ -9,23 +9,11 @@
 #   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #   License for the specific language governing permissions and limitations
 #   under the License.
-import inspect
-import os
+from ceilometerclient.openstack.common import version
 
 
-def _get_ceilometerclient_version():
-    """Read version from versioninfo file."""
-    mod_abspath = inspect.getabsfile(inspect.currentframe())
-    ceilometerclient_path = os.path.dirname(mod_abspath)
-    version_path = os.path.join(ceilometerclient_path, 'versioninfo')
-
-    if os.path.exists(version_path):
-        version = open(version_path).read().strip()
-    else:
-        version = "Unknown, couldn't find versioninfo file at %s"\
-                  % version_path
-
-    return version
-
-
-__version__ = _get_ceilometerclient_version()
+version_info = version.VersionInfo('python-ceilometerclient')
+try:
+    __version__ = version_info.version_string()
+except AttributeError:
+    __version__ = None

ceilometerclient/openstack/common/importutils.py
@@ -1,6 +1,6 @@
 # vim: tabstop=4 shiftwidth=4 softtabstop=4
 
-# Copyright 2011 OpenStack LLC.
+# Copyright 2011 OpenStack Foundation.
 # All Rights Reserved.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may

ceilometerclient/openstack/common/setup.py
@@ -1,6 +1,6 @@
 # vim: tabstop=4 shiftwidth=4 softtabstop=4
 
-# Copyright 2011 OpenStack LLC.
+# Copyright 2011 OpenStack Foundation.
 # Copyright 2012-2013 Hewlett-Packard Development Company, L.P.
 # All Rights Reserved.
 #
@@ -19,6 +19,8 @@
 """
 Utilities with minimum-depends for use in setup.py
 """
+
+from __future__ import print_function
 
 import email
 import os
@@ -41,6 +43,11 @@ def parse_mailmap(mailmap='.mailmap'):
                     continue
                 mapping[alias] = canonical_email
     return mapping
+
+
+def _parse_git_mailmap(git_dir, mailmap='.mailmap'):
+    mailmap = os.path.join(os.path.dirname(git_dir), mailmap)
+    return parse_mailmap(mailmap)
 
 
 def canonicalize_emails(changelog, mapping):
@@ -117,9 +124,9 @@ def _run_shell_command(cmd, throw_on_error=False):
         output = subprocess.Popen(["/bin/sh", "-c", cmd],
                                   stdout=subprocess.PIPE,
                                   stderr=subprocess.PIPE)
+    out = output.communicate()
     if output.returncode and throw_on_error:
         raise Exception("%s returned %d" % cmd, output.returncode)
-    out = output.communicate()
     if len(out) == 0:
         return None
     if len(out[0].strip()) == 0:
@@ -127,14 +134,26 @@ def _run_shell_command(cmd, throw_on_error=False):
     return out[0].strip()
 
 
+def _get_git_directory():
+    parent_dir = os.path.dirname(__file__)
+    while True:
+        git_dir = os.path.join(parent_dir, '.git')
+        if os.path.exists(git_dir):
+            return git_dir
+        parent_dir, child = os.path.split(parent_dir)
+        if not child:   # reached to root dir
+            return None
+
+
 def write_git_changelog():
     """Write a changelog based on the git changelog."""
     new_changelog = 'ChangeLog'
+    git_dir = _get_git_directory()
     if not os.getenv('SKIP_WRITE_GIT_CHANGELOG'):
-        if os.path.isdir('.git'):
-            git_log_cmd = 'git log --stat'
+        if git_dir:
+            git_log_cmd = 'git --git-dir=%s log' % git_dir
             changelog = _run_shell_command(git_log_cmd)
-            mailmap = parse_mailmap()
+            mailmap = _parse_git_mailmap(git_dir)
             with open(new_changelog, "w") as changelog_file:
                 changelog_file.write(canonicalize_emails(changelog, mailmap))
     else:
@@ -146,13 +165,23 @@ def generate_authors():
     jenkins_email = 'jenkins@review.(openstack|stackforge).org'
     old_authors = 'AUTHORS.in'
     new_authors = 'AUTHORS'
+    git_dir = _get_git_directory()
     if not os.getenv('SKIP_GENERATE_AUTHORS'):
-        if os.path.isdir('.git'):
+        if git_dir:
             # don't include jenkins email address in AUTHORS file
-            git_log_cmd = ("git log --format='%aN <%aE>' | sort -u | "
+            git_log_cmd = ("git --git-dir=" + git_dir +
+                           " log --format='%aN <%aE>' | sort -u | "
                            "egrep -v '" + jenkins_email + "'")
             changelog = _run_shell_command(git_log_cmd)
-            mailmap = parse_mailmap()
+            signed_cmd = ("git --git-dir=" + git_dir +
+                          " log | grep -i Co-authored-by: | sort -u")
+            signed_entries = _run_shell_command(signed_cmd)
+            if signed_entries:
+                new_entries = "\n".join(
+                    [signed.split(":", 1)[1].strip()
+                     for signed in signed_entries.split("\n") if signed])
+                changelog = "\n".join((changelog, new_entries))
+            mailmap = _parse_git_mailmap(git_dir)
             with open(new_authors, 'w') as new_authors_fh:
                 new_authors_fh.write(canonicalize_emails(changelog, mailmap))
                 if os.path.exists(old_authors):
@@ -205,7 +234,7 @@ def get_cmdclass():
             builders = ['html', 'man']
 
             def generate_autoindex(self):
-                print "**Autodocumenting from %s" % os.path.abspath(os.curdir)
+                print("**Autodocumenting from %s" % os.path.abspath(os.curdir))
                 modules = {}
                 option_dict = self.distribution.get_option_dict('build_sphinx')
                 source_dir = os.path.join(option_dict['source_dir'][1], 'api')
@@ -230,7 +259,7 @@ def get_cmdclass():
                         values = dict(module=module, heading=heading,
                                       underline=underline)
 
-                        print "Generating %s" % output_filename
+                        print("Generating %s" % output_filename)
                         with open(output_filename, 'w') as output_file:
                             output_file.write(_rst_template % values)
                         autoindex.write("   %s.rst\n" % module)
@@ -258,43 +287,48 @@ def get_cmdclass():
     return cmdclass
 
 
-def _get_revno():
+def _get_revno(git_dir):
     """Return the number of commits since the most recent tag.
 
     We use git-describe to find this out, but if there are no
     tags then we fall back to counting commits since the beginning
     of time.
     """
-    describe = _run_shell_command("git describe --always")
+    describe = _run_shell_command(
+        "git --git-dir=%s describe --always" % git_dir)
     if "-" in describe:
         return describe.rsplit("-", 2)[-2]
 
     # no tags found
-    revlist = _run_shell_command("git rev-list --abbrev-commit HEAD")
+    revlist = _run_shell_command(
+        "git --git-dir=%s rev-list --abbrev-commit HEAD" % git_dir)
     return len(revlist.splitlines())
 
 
-def get_version_from_git(pre_version):
+def _get_version_from_git(pre_version):
     """Return a version which is equal to the tag that's on the current
     revision if there is one, or tag plus number of additional revisions
     if the current revision has no tag."""
 
-    if os.path.isdir('.git'):
+    git_dir = _get_git_directory()
+    if git_dir:
         if pre_version:
             try:
                 return _run_shell_command(
-                    "git describe --exact-match",
+                    "git --git-dir=" + git_dir + " describe --exact-match",
                     throw_on_error=True).replace('-', '.')
             except Exception:
-                sha = _run_shell_command("git log -n1 --pretty=format:%h")
-                return "%s.a%s.g%s" % (pre_version, _get_revno(), sha)
+                sha = _run_shell_command(
+                    "git --git-dir=" + git_dir + " log -n1 --pretty=format:%h")
+                return "%s.a%s.g%s" % (pre_version, _get_revno(git_dir), sha)
         else:
             return _run_shell_command(
-                "git describe --always").replace('-', '.')
+                "git --git-dir=" + git_dir + " describe --always").replace(
+                    '-', '.')
     return None
 
 
-def get_version_from_pkg_info(package_name):
+def _get_version_from_pkg_info(package_name):
     """Get the version from PKG-INFO file if we can."""
     try:
         pkg_info_file = open('PKG-INFO', 'r')
@@ -325,10 +359,10 @@ def get_version(package_name, pre_version=None):
     version = os.environ.get("OSLO_PACKAGE_VERSION", None)
     if version:
         return version
-    version = get_version_from_pkg_info(package_name)
+    version = _get_version_from_pkg_info(package_name)
     if version:
         return version
-    version = get_version_from_git(pre_version)
+    version = _get_version_from_git(pre_version)
     if version:
         return version
     raise Exception("Versioning for this project requires either an sdist"

ceilometerclient/openstack/common/version.py
@@ -1,5 +1,5 @@
 
-#    Copyright 2012 OpenStack LLC
+#    Copyright 2012 OpenStack Foundation
 #    Copyright 2012-2013 Hewlett-Packard Development Company, L.P.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
@@ -49,11 +49,11 @@ class VersionInfo(object):
             provider = pkg_resources.get_provider(requirement)
             return provider.version
         except pkg_resources.DistributionNotFound:
-            # The most likely cause for this is running tests in a tree with
+            # The most likely cause for this is running tests in a tree
             # produced from a tarball where the package itself has not been
-            # installed into anything. Check for a PKG-INFO file.
+            # installed into anything. Revert to setup-time logic.
             from ceilometerclient.openstack.common import setup
-            return setup.get_version_from_pkg_info(self.package)
+            return setup.get_version(self.package)
 
     def release_string(self):
         """Return the full version of the package including suffixes indicating

commit 8bfc457b31054b0c3650121a1520958f36b17b43

Fix mis-scoped Client class breaking CLI
Fixes bug 1180439

The change in importation in this commit:

  https://github.com/openstack/python-ceilometerclient/commit/453f4d2a

caused the ceilometer CLI to fail in all cases with:

  'module' object has no attribute 'Client'

due to the mis-scoping of ceilometerclient.client.Client
as ceilometerclient.Client.

Change-Id: I5ec30bf74233f758519810629763ccb70bb7af87

ceilometerclient/shell.py
@@ -22,6 +22,7 @@ import sys
 from keystoneclient.v2_0 import client as ksclient
 
 import ceilometerclient
+from ceilometerclient import client as ceiloclient
 from ceilometerclient import exc
 from ceilometerclient.common import utils
 
@@ -302,7 +303,7 @@ class CeilometerShell(object):
             'key_file': args.key_file,
         }
 
-        client = ceilometerclient.Client(api_version, endpoint, **kwargs)
+        client = ceiloclient.Client(api_version, endpoint, **kwargs)
 
         try:
             args.func(client, args)