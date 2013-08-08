86e3ce177426b185cc9153fcb015add064cae4f1
Eoghan Glynn <eglynn@redhat.com>
2013-08-08 15:01:21 +0000

    Add support for new alarm repeat_actions attribute
    Continuous notification is generally required by Heat.
    
    Change-Id: Ib396f47c68298f883e305c755d821c2679a56a5c

--- ceilometerclient/v2/alarms.py
+++ ceilometerclient/v2/alarms.py
@@ -33,6 +33,7 @@ UPDATABLE_ATTRIBUTES = [
     'alarm_actions',
     'ok_actions',
     'insufficient_data_actions',
+    'repeat_actions',
     'matching_metadata',
     ]
 CREATION_ATTRIBUTES = UPDATABLE_ATTRIBUTES + ['name', 'project_id', 'user_id']
