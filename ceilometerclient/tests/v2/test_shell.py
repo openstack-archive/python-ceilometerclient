410ea25c567f866a7ff0f3938f482ac2b3711137
Kui Shi <skuicloud@gmail.com>
2013-10-14 06:48:14 +0800

    Import six.StringIO
    six.StringIO
    This is an fake file object for textual data. It is an alias for
    StringIO.StringIO in Python 2 and io.StringIO in Python 3.
    
    Partial Implement: blueprint py33-support
    
    Change-Id: I76c05041565614241eea7b7595e4503c88211ee8

--- ceilometerclient/tests/v2/test_shell.py
+++ ceilometerclient/tests/v2/test_shell.py
@@ -10,7 +10,7 @@
 #   License for the specific language governing permissions and limitations
 #   under the License.
 
-import cStringIO
+import six
 import mock
 import re
 import sys
@@ -102,7 +102,7 @@ class ShellAlarmHistoryCommandTest(utils.BaseTestCase):
     def _do_test_alarm_history(self, raw_query=None, parsed_query=None):
         self.args.query = raw_query
         orig = sys.stdout
-        sys.stdout = cStringIO.StringIO()
+        sys.stdout = six.StringIO()
         history = [alarms.AlarmChange(mock.Mock(), change)
                    for change in self.ALARM_HISTORY]
         self.cc.alarms.get_history.return_value = history
