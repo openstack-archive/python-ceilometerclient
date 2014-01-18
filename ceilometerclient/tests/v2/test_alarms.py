b9e072d5bc0deaadaadcdc8b9e945bc460ffd965
ls1175 <liusheng@huawei.com>
2014-01-18 15:21:31 +0800

    replace assertTrue(isinstance) to assertIsInstance
    some of tests use different method of assertTrue(isinstance(A, B))
    or assertEqual(type(A), B). The correct way is to use
    assertIsInstance(A, B) provided by testtools
    
    Change-Id: Idd931d27b13a46800e27393a98da096d41442320
    Closes-bug: #1268480

--- ceilometerclient/tests/v2/test_alarms.py
+++ ceilometerclient/tests/v2/test_alarms.py
@@ -323,7 +323,7 @@ class AlarmManagerTest(testtools.TestCase):
         self.assertEqual(self.api.calls, expect)
         for i in xrange(len(history)):
             change = history[i]
-            self.assertTrue(isinstance(change, alarms.AlarmChange))
+            self.bug/1268480(change, alarms.AlarmChange)
             for k, v in six.iteritems(ALARM_HISTORY[i]):
                 self.assertEqual(getattr(change, k), v)
 
