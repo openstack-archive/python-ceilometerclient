158dcd0265b473a137cfaf33496e8de74efb932e
Marc Solanas <marc@solanas.cat>
2014-01-21 10:02:27 -0800

    Checking the type of auth_token, and fixing it if necessary
    When creating a new Client object by calling the get_client function,
    if the ceilometer_url is not passed as a parameter, but the token is
    passed, the auth_token is converted to a lambda function.
    However, when both the ceilometer_url and the auth_token are passed,
    this conversion does not take place.
    This fix checks if the auth_token is a lambda function or not.
    If it is not, it creates a new lambda function which returns the
    token.
    Without this check, if a token was passed as a string, the
    python client would throw and error, when trying to call
    self.auth_token() in ceilomenterclient/common/http.py.
    
    Change-Id: I3e564fcf17ce497546b75f0ab8c7ca0f6f2099df
    Closes-Bug: #1254186

--- ceilometerclient/client.py
+++ ceilometerclient/client.py
@@ -10,9 +10,10 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from ceilometerclient.common import utils
 from keystoneclient.v2_0 import client as ksclient
+import six
 
+from ceilometerclient.common import utils
 
 def _get_ksclient(**kwargs):
     """Get an endpoint and auth token from Keystone.
@@ -60,6 +61,7 @@ def get_client(api_version, **kwargs):
     """
     if kwargs.get('os_auth_token') and kwargs.get('ceilometer_url'):
         token = kwargs.get('os_auth_token')
+        token = (token if six.callable(token) else lambda: token)
         endpoint = kwargs.get('ceilometer_url')
     elif (kwargs.get('os_username') and
           kwargs.get('os_password') and
