4ad96c6f193a788249d27045f1d28daa6e56be82
liu-sheng <liusheng@huawei.com>
2014-03-04 10:30:11 +0800

    Correct help string of alarm-create
    Replace insufficient_data to insufficient data in paramete
    r "--state" of alarm-create help sting.
    
    Change-Id: I3f336171055fc26a4529a58092b4d9a9616794a4

--- ceilometerclient/v2/shell.py
+++ ceilometerclient/v2/shell.py
@@ -26,7 +26,7 @@ from ceilometerclient.openstack.common import strutils
 from ceilometerclient.v2 import options
 
 
-ALARM_STATES = ['ok', 'alarm', 'insufficient_data']
+ALARM_STATES = ['ok', 'alarm', 'insufficient data']
 ALARM_OPERATORS = ['lt', 'le', 'eq', 'ne', 'ge', 'gt']
 ALARM_COMBINATION_OPERATORS = ['and', 'or']
 STATISTICS = ['max', 'min', 'avg', 'sum', 'count']
@@ -286,7 +286,7 @@ def common_alarm_arguments(create=False):
                    dest='insufficient_data_actions',
                    metavar='<Webhook URL>', action='append', default=None,
                    help=('URL to invoke when state transitions to '
-                         'insufficient_data. May be used multiple times.'))
+                         'insufficient data. May be used multiple times.'))
         @functools.wraps(func)
         def _wrapped(*args, **kwargs):
             return func(*args, **kwargs)
