b7879b4ec424fa077a60ef31c1434004c0efd7d9
Stéphane Albert <stephane.albert@objectif-libre.com>
2014-03-13 12:42:06 +0100

    Statistics groupby handling improvement
    The groupby parameter for statistics now handle single string as a
    parameter. It fixes the bug where every characters of the string where
    interpreted as a groupby value.
    
    Change-Id: Ia763741d7ad5717be381b0ac2f0acb57e9f46cac
    Closes-Bug: #1291935

--- ceilometerclient/tests/v2/test_statistics.py
+++ ceilometerclient/tests/v2/test_statistics.py
@@ -156,3 +156,23 @@ class StatisticsManagerTest(utils.BaseTestCase):
         self.assertEqual(stats[1].count, 12)
         self.assertEqual(stats[0].groupby.get('resource_id'), 'foo')
         self.assertEqual(stats[1].groupby.get('resource_id'), 'bar')
+
+    def test_list_by_meter_name_with_groupby_as_str(self):
+        stats = list(self.mgr.list(meter_name='instance',
+                                   q=[
+                                       {"field": "resource_id",
+                                        "value": "foo"},
+                                       {"field": "source",
+                                        "value": "bar"},
+                                   ],
+                                   groupby='resource_id'))
+        expect = [
+            ('GET',
+             '%s?%s%s' % (base_url, qry, groupby), {}, None),
+        ]
+        self.assertEqual(self.api.calls, expect)
+        self.assertEqual(len(stats), 2)
+        self.assertEqual(stats[0].count, 135)
+        self.assertEqual(stats[1].count, 12)
+        self.assertEqual(stats[0].groupby.get('resource_id'), 'foo')
+        self.assertEqual(stats[1].groupby.get('resource_id'), 'bar')
