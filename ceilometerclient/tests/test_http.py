5ebca0399f68c43439e99529784b7dc789df7113
Cristian A Sanchez <cristian.a.sanchez@intel.com>
2014-05-26 13:59:37 -0300

    Make HTTP client aware of no_proxy variable
    Fix the problem of ceilometer client ignoring the host list in
    no_proxy environment variable. Now proxy url is only used when
    the Host of target URL is not in the no_proxy host list.
    
    Closes-Bug: #1310805
    Change-Id: I0b841291f1e8b9d9c7f431efd7d8f95d37df72b6

--- ceilometerclient/tests/test_http.py
+++ ceilometerclient/tests/test_http.py
@@ -17,6 +17,7 @@ import mock
 
 from ceilometerclient.common import http
 from ceilometerclient.tests import utils
+from six.moves.urllib import request
 
 
 class HttpClientTest(utils.BaseTestCase):
@@ -46,19 +47,38 @@ class HttpClientTest(utils.BaseTestCase):
         client = http.HTTPClient(self.url)
         self.assertIsNotNone(client.get_connection())
 
+    @mock.patch.object(request, 'proxy_bypass')
     @mock.patch.object(http.HTTPClient, 'get_connection')
-    def test_url_generation_with_proxy(self, get_conn):
-        client = http.HTTPClient(self.url, token=lambda: 'token')
-        client.proxy_url = "http://localhost:3128/"
+    def test_url_generation_with_proxy(self, get_conn, proxy_bypass):
+        proxy_bypass.return_value = False
         conn = mock.MagicMock()
         conn.request.side_effect = Exception("stop")
         get_conn.return_value = conn
+        client = http.HTTPClient(self.url, token=lambda: 'token')
+        client.proxy_url = "http://localhost:3128/"
         try:
             client._http_request('/v1/resources', 'GET')
         except Exception:
             pass
         conn.request.assert_called_once_with('GET', (self.url.rstrip('/') +
                                                      '/v1/resources'),
+                                             headers=mock.ANY)
+
+    @mock.patch.object(request, 'proxy_bypass')
+    @mock.patch.object(http.HTTPClient, 'get_connection')
+    def test_url_generation_without_proxy_for_no_proxy_host(self, get_conn,
+                                                            proxy_bypass):
+        proxy_bypass.return_value = True
+        conn = mock.MagicMock()
+        conn.request.side_effect = Exception("stop")
+        get_conn.return_value = conn
+        client = http.HTTPClient(self.url, token=lambda: 'token')
+        client.proxy_url = "http://proxy:3128/"
+        try:
+            client._http_request('/v1/resources', 'GET')
+        except Exception:
+            pass
+        conn.request.assert_called_once_with('GET', '/v1/resources',
                                              headers=mock.ANY)
 
 
