7290094c99e36ce92e0b0e3ff18fedc3471b9b47
Cristian A Sanchez <cristian.a.sanchez@intel.com>
2014-05-26 13:59:37 -0300

    Make HTTP client aware of no_proxy variable
    Fix the problem of ceilometer client ignoring the host list in
    no_proxy environment variable. Now proxy url is only used when
    the Host of target URL is not in the no_proxy host list.
    
    Closes-Bug: #1310805
    Change-Id: I0b841291f1e8b9d9c7f431efd7d8f95d37df72b6

--- ceilometerclient/common/http.py
+++ ceilometerclient/common/http.py
@@ -48,6 +48,7 @@ class HTTPClient(object):
         self.auth_token = kwargs.get('token')
         self.connection_params = self.get_connection_params(endpoint, **kwargs)
         self.proxy_url = self.get_proxy_url()
+        self.use_proxy = self._use_proxy()
 
     @staticmethod
     def get_connection_params(endpoint, **kwargs):
@@ -74,7 +75,7 @@ class HTTPClient(object):
     def get_connection(self):
         _class = self.connection_params[0]
         try:
-            if self.proxy_url:
+            if self.use_proxy:
                 proxy_parts = urllib.parse.urlparse(self.proxy_url)
                 return _class(proxy_parts.hostname, proxy_parts.port,
                               **self.connection_params[2])
@@ -142,7 +143,7 @@ class HTTPClient(object):
         conn = self.get_connection()
 
         try:
-            if self.proxy_url:
+            if self.use_proxy:
                 conn_url = (self.endpoint.rstrip('/') +
                             self._make_connection_url(url))
             else:
@@ -219,6 +220,10 @@ class HTTPClient(object):
             return os.environ.get('http_proxy')
         msg = 'Unsupported scheme: %s' % scheme
         raise exc.InvalidEndpoint(msg)
+
+    def _use_proxy(self):
+        host = self.connection_params[1][0]
+        return self.proxy_url and not urllib.request.proxy_bypass(host)
 
 
 class VerifiedHTTPSConnection(httplib.HTTPSConnection):
