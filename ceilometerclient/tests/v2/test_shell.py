8b525bba49fc979cd501e69904be97a9ef03f9ce
Nejc Saje <nejc.saje@xlab.si>
2014-05-27 10:21:27 +0000

    Check if the alarm has time constraints field before displaying
    Fixes the bug that broke the alarms CLI if the alarm didn't have
    a time constraints field.
    
    Reduces code duplication of alarm printing code, so now all the alarm
    printing code is actually tested.
    
    Renames some auxiliary methods for more clarity.
    
    Change-Id: Ib691b4a5a6cf5ae133cd0a5576f90e4d0d189a92
    Closes-bug: #1316390

--- ceilometerclient/tests/v2/test_shell.py
+++ ceilometerclient/tests/v2/test_shell.py
@@ -516,6 +516,8 @@ class ShellQueryAlarmsCommandTest(utils.BaseTestCase):
               "state_timestamp": "2014-02-20T10:37:15.589860",
               "threshold_rule": None,
               "timestamp": "2014-02-20T10:37:15.589856",
+              "time_constraints": [{"name": "test", "start": "0 23 * * *",
+                                    "duration": 10800}],
               "type": "combination",
               "user_id": "c96c887c216949acbdfbd8b494863567"}]
 
@@ -543,19 +545,52 @@ class ShellQueryAlarmsCommandTest(utils.BaseTestCase):
         self.assertEqual('''\
 +--------------------------------------+------------------+-------+---------\
 +------------+--------------------------------------------------------------\
-----------------------------------------+
+----------------------------------------+--------------------------------+
 | Alarm ID                             | Name             | State | Enabled \
 | Continuous | Alarm condition                                              \
-                                        |
+                                        | Time constraints               |
 +--------------------------------------+------------------+-------+---------\
 +------------+--------------------------------------------------------------\
-----------------------------------------+
+----------------------------------------+--------------------------------+
 | 768ff714-8cfb-4db9-9753-d484cb33a1cc | SwiftObjectAlarm | ok    | True    \
 | False      | combinated states (OR) of 739e99cb-c2ec-4718-b900-332502355f3\
-8, 153462d0-a9b8-4b5b-8175-9e4b05e9b856 |
+8, 153462d0-a9b8-4b5b-8175-9e4b05e9b856 | test at 0 23 * * *  for 10800s |
 +--------------------------------------+------------------+-------+---------\
 +------------+--------------------------------------------------------------\
-----------------------------------------+
+----------------------------------------+--------------------------------+
+''', sys.stdout.getvalue())
+
+    @mock.patch('sys.stdout', new=six.StringIO())
+    def test_time_constraints_compatibility(self):
+        # client should be backwards compatible
+        alarm_without_tc = dict(self.ALARM[0])
+        del alarm_without_tc['time_constraints']
+
+        # NOTE(nsaje): Since we're accessing a nonexisting key in the resource,
+        # the resource is looking it up with the manager (which is a mock).
+        manager_mock = mock.Mock()
+        del manager_mock.get
+        ret_alarm = [alarms.Alarm(manager_mock, alarm_without_tc)]
+        self.cc.query_alarms.query.return_value = ret_alarm
+
+        ceilometer_shell.do_query_alarms(self.cc, self.args)
+
+        self.assertEqual('''\
++--------------------------------------+------------------+-------+---------\
++------------+--------------------------------------------------------------\
+----------------------------------------+------------------+
+| Alarm ID                             | Name             | State | Enabled \
+| Continuous | Alarm condition                                              \
+                                        | Time constraints |
++--------------------------------------+------------------+-------+---------\
++------------+--------------------------------------------------------------\
+----------------------------------------+------------------+
+| 768ff714-8cfb-4db9-9753-d484cb33a1cc | SwiftObjectAlarm | ok    | True    \
+| False      | combinated states (OR) of 739e99cb-c2ec-4718-b900-332502355f3\
+8, 153462d0-a9b8-4b5b-8175-9e4b05e9b856 | None             |
++--------------------------------------+------------------+-------+---------\
++------------+--------------------------------------------------------------\
+----------------------------------------+------------------+
 ''', sys.stdout.getvalue())
 
 
