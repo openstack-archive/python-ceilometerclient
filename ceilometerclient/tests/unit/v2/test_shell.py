975d48dd4cc67eb7dd48a583d9afad984bc88c3a
xialinjuan <ljxiash@cn.ibm.com>
2015-11-26 11:19:23 +0800

    fix project_id and user_id fields not set when create gnocchi alarm
    When using command alarm-gnocchi-* the project_id and user_id are not
    set.
    
    Closes-Bug: #1506509
    Change-Id: If6a8cfd9bc52b3eeff99dfcfdee2f4da7220674b

--- ceilometerclient/tests/unit/v2/test_shell.py
+++ ceilometerclient/tests/unit/v2/test_shell.py
@@ -1264,6 +1264,52 @@ class ShellShadowedArgsTest(test_shell.ShellTestBase):
         self._test_shadowed_args_alarm('alarm-combination-update',
                                        cli_args, 'update')
 
+    def test_shadowed_args_gnocchi_resources_threshold_alarm(self):
+        cli_args = [
+            '--metric', 'cpu',
+            '--threshold', '80',
+            '--resource-type', 'instance',
+            '--resource-id', 'fb16a05a-669d-414e-8bbe-93aa381df6a8',
+            '--aggregation-method', 'last',
+        ]
+        self._test_shadowed_args_alarm('alarm-gnocchi-resources-'
+                                       'threshold-create',
+                                       cli_args, 'create')
+        cli_args += ['--alarm_id', '437b7ed0-3733-4054-a877-e9a297b8be85']
+        self._test_shadowed_args_alarm('alarm-gnocchi-resources-'
+                                       'threshold-update',
+                                       cli_args, 'update')
+
+    def test_shadowed_args_gnocchi_aggr_by_resources_threshold_alarm(self):
+        cli_args = [
+            '--metric', 'cpu',
+            '--threshold', '80',
+            '--resource-type', 'instance',
+            '--aggregation-method', 'last',
+            '--query', '"server_group":"my_autoscaling_group"',
+        ]
+        self._test_shadowed_args_alarm('alarm-gnocchi-aggregation-'
+                                       'by-resources-threshold-create',
+                                       cli_args, 'create')
+        cli_args += ['--alarm_id', '437b7ed0-3733-4054-a877-e9a297b8be85']
+        self._test_shadowed_args_alarm('alarm-gnocchi-aggregation-'
+                                       'by-resources-threshold-update',
+                                       cli_args, 'update')
+
+    def test_shadowed_args_gnocchi_aggr_by_metrics_threshold_alarm(self):
+        cli_args = [
+            '--metric', 'cpu,cpu_util',
+            '--threshold', '80',
+            '--aggregation-method', 'last',
+        ]
+        self._test_shadowed_args_alarm('alarm-gnocchi-aggregation-'
+                                       'by-metrics-threshold-create',
+                                       cli_args, 'create')
+        cli_args += ['--alarm_id', '437b7ed0-3733-4054-a877-e9a297b8be85']
+        self._test_shadowed_args_alarm('alarm-gnocchi-aggregation-'
+                                       'by-metrics-threshold-update',
+                                       cli_args, 'update')
+
     @mock.patch.object(samples.OldSampleManager, 'create')
     def test_shadowed_args_sample_create(self, mocked):
         self.make_env(test_shell.FAKE_V2_ENV)
