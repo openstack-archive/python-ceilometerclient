aaddc52dbac711322c36966ea5fea75236e36468
Hanxi Liu <hanxi.liu@easystack.cn>
2017-06-23 11:11:39 +0800

    Docs: switch to openstackdocstheme
    Project documents are being reprocessed due to document migration[1].
    Update the docs theme to openstackdocstheme instead of oslosphinx.
    
    [1] https://review.openstack.org/#/c/472275/
    
    Change-Id: Ic24ad9d7759ab9169bcc68db8c6d79e4bc69a76b
    Co-Authored-By: chenxing <chen.xing@99cloud.net>

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -11,6 +11,7 @@
 # under the License.
 
 import os
+
 execfile(os.path.join("..", "ext", "gen_ref.py"))
 
 project = 'python-ceilometerclient'
@@ -24,7 +25,7 @@ gen_ref("v2", "Version 2 API Reference",
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'oslosphinx' ]
+extensions = ['sphinx.ext.autodoc', 'openstackdocstheme']
 
 # autodoc generation is a bit aggressive and a nuisance when doing heavy
 # text edit cycles.
@@ -57,6 +58,10 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  Major themes that come with
 # Sphinx are currently 'default' and 'sphinxdoc'.
 #html_theme = 'nature'
+html_theme = 'openstackdocs'
+
+# Add any paths that contain custom themes here, relative to this directory.
+#html_theme_path = ['_theme']
 
 # Output file base name for HTML help builder.
 htmlhelp_basename = '%sdoc' % project
