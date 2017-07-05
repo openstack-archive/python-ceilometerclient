3a70f87e33dd545d31d4704cec7bbd936e039b9d
lingyongxu <lyxu@fiberhome.com>
2017-07-05 16:25:59 +0800

    Switch from oslosphinx to openstackdocstheme
    To use openstackdocstheme 1.11.0 properly, this patch fixes
    some settings according to follow.
    https://docs.openstack.org/openstackdocstheme/latest/
    
    Change-Id: I140e9bcbe876b935e74a868234dcef903a732da5

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -11,6 +11,8 @@
 # under the License.
 
 import os
+import openstackdocstheme
+
 execfile(os.path.join("..", "ext", "gen_ref.py"))
 
 project = 'python-ceilometerclient'
@@ -24,7 +26,15 @@ gen_ref("v2", "Version 2 API Reference",
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'oslosphinx' ]
+extensions = ['sphinx.ext.autodoc', 'openstackdocstheme' ]
+
+# openstackdocstheme options
+repository_name = 'openstack/python-ceilometerclient'
+bug_project = 'python-ceilometerclient'
+bug_tag = ''
+
+# Must set this variable to include year, month, day, hours, and minutes.
+html_last_updated_fmt = '%Y-%m-%d %H:%M'
 
 # autodoc generation is a bit aggressive and a nuisance when doing heavy
 # text edit cycles.
--- releasenotes/source/conf.py
+++ releasenotes/source/conf.py
@@ -38,9 +38,17 @@
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'oslosphinx',
+    'openstackdocstheme',
     'reno.sphinxext',
 ]
+
+# openstackdocstheme options
+repository_name = 'openstack/python-ceilometerclient'
+bug_project = 'python-ceilometerclient'
+bug_tag = ''
+
+# Must set this variable to include year, month, day, hours, and minutes.
+html_last_updated_fmt = '%Y-%m-%d %H:%M'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
