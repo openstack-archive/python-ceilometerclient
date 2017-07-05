3f1f54a1863dc9af38645fb9dcd3a92344199659
lingyongxu <lyxu@fiberhome.com>
2017-07-05 16:25:59 +0800

    Switch from oslosphinx to openstackdocstheme
    To use openstackdocstheme 1.11.0 properly, this patch fixes
    some settings according to follow.
    https://docs.openstack.org/openstackdocstheme/latest/
    
    Change-Id: I140e9bcbe876b935e74a868234dcef903a732da5

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -24,7 +24,15 @@ gen_ref("v2", "Version 2 API Reference",
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'oslosphinx' ]
+extensions = ['sphinx.ext.autodoc', 'openstackdocstheme' ]
+
+# openstackdocstheme options
+repository_name = 'openstack/python-ceilometerclient'
+bug_project = 'python-ceilometerclient'
+bug_tag = ''
+
+# Must set this variable to include year, month, day, hours, and minutes.
+html_last_updated_fmt = '%Y-%m-%d %H:%M'
 
 # autodoc generation is a bit aggressive and a nuisance when doing heavy
 # text edit cycles.
